d706ef190616744ad71fe912c86e38bc7e842654
pleaseuma79-code <pleaseuma79@gmail.com>
2026-02-08 13:18:16 +0600

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const pool = new Pool({
   host: process.env.PGHOST,
